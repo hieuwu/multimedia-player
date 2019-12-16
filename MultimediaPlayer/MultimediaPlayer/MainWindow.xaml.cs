e7ee26660b81a33b8b3c54e769591b88df66b555
hieuwu <43868345+hieuwu@users.noreply.github.com>
2019-12-16 16:55:49 +0700

    Created interface with buttons to play and background color

--- MultimediaPlayer/MultimediaPlayer/MainWindow.xaml.cs
+++ MultimediaPlayer/MultimediaPlayer/MainWindow.xaml.cs
@@ -24,5 +24,59 @@ namespace MultimediaPlayer
         {
             InitializeComponent();
         }
+
+        private void BtnPlayClick(object sender, RoutedEventArgs e)
+        {
+            btnPause.Visibility = Visibility.Visible;
+            btnPlay.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnPauseClick(object sender, RoutedEventArgs e)
+        {
+            btnPlay.Visibility = Visibility.Visible;
+            btnPause.Visibility = Visibility.Hidden;
+
+        }
+
+        private void BtnPreClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnNextClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnShuffleEnableClick(object sender, RoutedEventArgs e)
+        {
+            btnShuffleEnable.Visibility = Visibility.Hidden;
+            btnShuffleDisable.Visibility = Visibility.Visible;
+        }
+
+        private void BtnShuffleDisableClick(object sender, RoutedEventArgs e)
+        {
+
+            btnShuffleDisable.Visibility = Visibility.Hidden;
+            btnShuffleEnable.Visibility = Visibility.Visible;
+        }
+
+        private void BtnLoopAllClick(object sender, RoutedEventArgs e)
+        {
+            btnLoopAll.Visibility = Visibility.Hidden;
+            btnLoopOne.Visibility = Visibility.Visible;
+        }
+
+        private void BtnLoopDisableClick(object sender, RoutedEventArgs e)
+        {
+            btnLoopDisable.Visibility = Visibility.Hidden;
+            btnLoopAll.Visibility = Visibility.Visible;
+        }
+
+        private void BtnLoopOneClick(object sender, RoutedEventArgs e)
+        {
+            btnLoopOne.Visibility = Visibility.Hidden;
+            btnLoopDisable.Visibility = Visibility.Visible;
+        }
     }
 }
